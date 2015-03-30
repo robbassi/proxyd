ba9be9b971c42126c2886b2e2989b64c45339268
Rob Bassi <rbassi@venasolutions.com>
2015-03-30 19:44:31 -0400

    Cleanup

--- server.h
+++ server.h
@@ -1,19 +1,5 @@
 #ifndef __SERVER_H__
 #define __SERVER_H__ 1
 
-#include <stdbool.h>
-
-struct socks5_session
-{
-  bool authenticated;
-  bool active;
-  struct tcpConnection *client;
-  struct tcpConnection *server;
-};
-
-extern char *socks5_auth_method_desc[];
-extern char *socks5_command_desc[];
-extern char *socks5_addr_type_desc[];
-extern char *socks5_response_code_desc[];
 
 #endif /* __SERVER_H__ */
